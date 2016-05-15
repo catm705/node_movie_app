995cf13557bb778266783a934d0b40c71bbad320
Cat Gelinas <cat@bedrocket.com>
2016-05-15 18:42:57 -0400

    Change header colors in title

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,11 @@
 var xhr;
+var int;
 var movieUrl = 'https://www.omdbapi.com/?';
 var movieList;
 
+function blink() {
+  document.getElementById( 'load-icon' ).classList.toggle( 'hidden' );
+}
 
 var search = function() {
   var searchTerm = document.getElementById( 'title' ).value;
@@ -25,61 +29,66 @@ function getMovies( url ) {
 }
 
 function displayContent() {
+  int = setInterval( blink, 500 );
+  console.log("int: ", int );
+
   if ( xhr.readyState === XMLHttpRequest.DONE ) {
     if ( xhr.status === 200 ) {
+
       movieList = JSON.parse( xhr.responseText );
 
       var parentElement = document.getElementById( 'movies' );
 
       var objKeys = ['Title', 'Year' ];
 
-        for ( i = 0; i <= movieList[ 'Search' ].length - 1; i++ ) {
-          var form = document.createElement("form");
-          var element1 = document.createElement("input");
-          element1.className += "first";
+      for ( i = 0; i <= movieList[ 'Search' ].length - 1; i++ ) {
+        var form = document.createElement("form");
+        var element1 = document.createElement("input");
+        element1.className += "first";
 
-          var element2 = document.createElement("input");
-          form.action = "/insert";
-          form.method = "POST";
-          element2.className += "second";
+        var element2 = document.createElement("input");
+        form.action = "/insert";
+        form.method = "POST";
+        element2.className += "second";
 
-          parentElement.appendChild( form );
-          form.appendChild( element1 );
-          form.appendChild( element2 );
+        parentElement.appendChild( form );
+        form.appendChild( element1 );
+        form.appendChild( element2 );
 
-          for ( var k in movieList[ 'Search' ][ i ] ) {
-            for ( j = 0; j <= objKeys.length - 1; j ++ ) {
-              if ( k == objKeys[ j ] ) {
-                if ( k == "Title" ) {
-                  value1 = movieList[ 'Search'][ i ][ k ];
-                  element1.name = "Title";
-                  element1.id = "Title";
-                  element1.value = value1;
-                }
+        for ( var k in movieList[ 'Search' ][ i ] ) {
+          for ( j = 0; j <= objKeys.length - 1; j ++ ) {
+            if ( k == objKeys[ j ] ) {
+              if ( k == "Title" ) {
+                value1 = movieList[ 'Search'][ i ][ k ];
+                element1.name = "Title";
+                element1.id = "Title";
+                element1.value = value1;
+              }
 
-                if ( k == "Year" ) {
-                  value2 = movieList[ 'Search'][ i ][ k ];
-                  element2.name = "Year";
-                  element2.id = "Year";
-                  element2.value = value2;
+              if ( k == "Year" ) {
+                value2 = movieList[ 'Search'][ i ][ k ];
+                element2.name = "Year";
+                element2.id = "Year";
+                element2.value = value2;
 
-                  var button = document.createElement("button");
-                  button.setAttribute( 'type', 'submit' );
-                  button.className = 'favorite';
-                  button.innerText = 'Add to Favs';
+                var button = document.createElement("button");
+                button.setAttribute( 'type', 'submit' );
+                button.className = 'favorite';
+                button.innerText = 'Add to Favs';
 
-                  form.appendChild( button );
-                }
+                form.appendChild( button );
               }
             }
           }
         }
-
-      return movieList;
+      }
     }
     else {
       console.log("There was a problem with the request.", xhr.status);
     }
+
+    window.clearInterval( int );
+    console.log("am I done?");
   }
 }
 
